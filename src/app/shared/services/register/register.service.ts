c460eb073c43aebdbc8cd4a6871f3e6d0926b29b
Mbarak Mbigo <mbarak.mbigo@andela.com>
2017-05-18 15:45:51 +0300

    add application models: user, bucketlist, bucketitem

--- src/app/shared/services/register/register.service.ts
+++ src/app/shared/services/register/register.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Http,Response, Headers  } from '@angular/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class RegisterService {

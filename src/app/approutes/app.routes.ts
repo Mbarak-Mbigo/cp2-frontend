607f07d2ff5a950446193ba48b9872c8b8c87121
Mbarak Mbigo <mbarak.mbigo@andela.com>
2017-05-18 10:49:00 +0300

    add login route

--- src/app/approutes/app.routes.ts
+++ src/app/approutes/app.routes.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes }  from '@angular/router';
 
 // local imports
 import { HomeComponent } from '../core/base/home/home.component';
+import { LoginComponent } from '../core/base/login/login.component';
 
 
 // Routes
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent}
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent }
 ]
 
 // export routes as a module

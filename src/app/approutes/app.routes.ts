5ef89f32722c526d0be49e5f88ffc455558ebe72
Mbarak Mbigo <mbarak.mbigo@andela.com>
2017-05-18 10:54:56 +0300

    add pagenotfound route

--- src/app/approutes/app.routes.ts
+++ src/app/approutes/app.routes.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes }  from '@angular/router';
 import { HomeComponent } from '../core/base/home/home.component';
 import { LoginComponent } from '../core/base/login/login.component';
 import { RegisterComponent } from '../core/base/register/register.component';
+import { PagenotfoundComponent } from '../core/base/pagenotfound/pagenotfound.component';
 
 
 // Routes
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  {path: '**', component: PagenotfoundComponent }
 ]
 
 // export routes as a module

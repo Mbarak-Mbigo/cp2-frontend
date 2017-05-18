b296f7bbb21068a60305ccbb41deaf9ed41d1111
Mbarak Mbigo <mbarak.mbigo@andela.com>
2017-05-18 15:36:32 +0300

    add registration ui styling

--- src/app/core/base/register/register.component.ts
+++ src/app/core/base/register/register.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder , Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  // providers: [RegisterService]
 })
 export class RegisterComponent implements OnInit {
   registerTitle: string = 'Register View'
-  constructor() { }
+  loading = false;
+  userRegisterForm: FormGroup;
+  errorMessage: string;
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit() {
+    this.userRegisterForm = this.formBuilder.group({
+      username: [null, [Validators.required]],
+      email: [null, [Validators.required]],
+      password: [null, [Validators.required]]
+    })
   }
 
 }

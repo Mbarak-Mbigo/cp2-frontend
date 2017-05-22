8af8707d4ac0dee89cebed3ebcd504b3a51425b5
Mbarak Mbigo <mbarak.mbigo@andela.com>
2017-05-22 18:41:47 +0300

    refactor code: add bucketlist service error handling

--- src/app/core/features/modals/bucketlistmodal/bucketlistmodal.component.ts
+++ src/app/core/features/modals/bucketlistmodal/bucketlistmodal.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
 
 import { AlertService } from '../../../../shared/services/alert/alert.service';
-declare var $:any;
 
 @Component({
   selector: 'app-bucketlistmodal',
@@ -12,7 +10,6 @@ declare var $:any;
 })
 
 export class BucketlistmodalComponent implements OnInit {
-  subscription: Subscription;
   @Output() deleteBucket = new EventEmitter();
   @Output() updateBucket = new EventEmitter();
   @Output() createBucket = new EventEmitter();
@@ -20,30 +17,16 @@ export class BucketlistmodalComponent implements OnInit {
   @Input() bucketUrl: string;
   @Input() bucketName: string;
 
-  constructor(private alertService: AlertService) {
-    // subscribe to alertservice component messages
-    this.subscription = this.alertService.alertCreateBucketList()
-    .subscribe(
-      alertSignal =>{
-        let bucketId = '#createBucket';
-        $(bucketId).modal('show');
-      }
-    )
-  }
+  constructor(private alertService: AlertService) {}
 
   ngOnInit() {
   }
 
   deleteBucketList(bucketUrl: string){
-    this.deleteBucket.emit({$event: event, data: bucketUrl})
+    this.deleteBucket.emit({$event: event, data: bucketUrl});
   }
 
   updateBucketList(editData: string){
-    this.updateBucket.emit({$event: event, data: editData, bucketUrl: this.bucketUrl})
+    this.updateBucket.emit({$event: event, data: editData, bucketUrl: this.bucketUrl});
   }
-
-  createBucketList(data: string){
-    this.createBucket.emit({$event: event, data: data})
-  }
-
 }
